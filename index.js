e9dc520ab4d234a844af2db776764e6c7deb9dba
Carol Bernadette <carolbernadette35@gmail.com>
2024-06-24 17:24:51 +0300

    counter

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
 function incrementcount() {
     count += 1
 countEl.innerText = count ;
 }
 function save(){
-    console.log(count)
+
+let sd = count + " - "
+saveEl.textContent += sd
+countEl.textContent = 0
+count = 0
 }
